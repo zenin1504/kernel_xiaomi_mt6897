9483b12dcd293df01c903d1c9ad3cadb4be884cb
rio004 <riyon336@gmail.com>
2024-10-09 13:10:38 +0100

    drivers: media: platform: Import MediaTek changes
    * Also from https://github.com/MiCode/mtkcam-kernel_device_modules/tree/bsp-duchamp-u-oss
    
    Change-Id: I0c245fd74e7f7e3b808dbbd1f3df582387fd2397
    Signed-off-by: rio004 <riyon336@gmail.com>

---
+++ drivers/media/platform/mediatek/isp/imgsys/platforms/isp7sp/modules/mtk_imgsys-me.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ *
+ * Author: Marvin Lin <Marvin.Lin@mediatek.com>
+ *
+ */
+
+#ifndef _MTK_IMGSYS_ME_H_
+#define _MTK_IMGSYS_ME_H_
+
+#include "mtk_imgsys-dev.h"
+#include "./../mtk_imgsys-debug.h"
+
+#define ME_CTL_OFFSET      0x0000
+#define ME_CTL_RANGE       0xA10
+#define ME_CTL_RANGE_TF    0x120
+
+#define MMG_CTL_OFFSET      0x0000
+#define MMG_CTL_RANGE       0xA40
+#define MMG_CTL_RANGE_TF    0x30
+
+void imgsys_me_set_initial_value(struct mtk_imgsys_dev *imgsys_dev);
+void imgsys_me_debug_dump(struct mtk_imgsys_dev *imgsys_dev,
+							unsigned int engine);
+void imgsys_me_uninit(struct mtk_imgsys_dev *imgsys_dev);
+void ME_mode3_reset(struct mtk_imgsys_dev *imgsys_dev);
+int ME_TranslationFault_callback(int port, dma_addr_t mva, void *data);
+int MMG_TranslationFault_callback(int port, dma_addr_t mva, void *data);
+
+#endif /* _MTK_IMGSYS_ME_H_ */
